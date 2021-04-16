551188ae278864acdc758ee8f07ebf5589e319cc
Wan Muzaffar <wanmuzaffar@Wans-MacBook-Pro.local>
2021-04-16 14:50:11 +0800

    add item

---
+++ MyFilm/DetailViewController.swift
@@ -0,0 +1,33 @@
+//
+//  DetailViewController.swift
+//  MyFilm
+//
+//  Created by Wan Muzaffar on 16/04/2021.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    @IBOutlet weak var actorLabel: UILabel!
+    @IBOutlet weak var yearLable: UILabel!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- MyFilm/ViewController.swift
+++ MyFilm/ViewController.swift
@@ -7,13 +7,37 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = "Hello World"
+        return cell
+    }
+    
 
+    @IBOutlet weak var searchTextField: UITextField!
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func searchPressed(_ sender: Any) {
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "toDetail" {
+        let destVC = segue.destination as! DetailViewController
+        let selectedIndexPath = tableView.indexPathForSelectedRow!
+//        let selectedItem = movies[selectedIndexPath.row]
+//        destVC.movieId  = selectedItem
+        }
+    }
 }
 
